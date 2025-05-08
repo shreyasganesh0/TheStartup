36cff95003c56b04199669e84027f9b79c284bed
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-05-08 08:03:14 -0400

    Add byte parsing

--- main.go
+++ main.go
@@ -2,9 +2,51 @@ package main
 
 import( 
 	"fmt"
+	"os"
+	"io"
+	"bytes"
 )
 
 func main() {
 
-	fmt.Print("I hope I get the job!\n");
+
+	fd, err := os.Open("messages.txt");
+	if (err != nil) {
+
+		fmt.Printf("Error opening file %z\n", err);
+	}
+
+	chars := make([]byte, 8);
+	tmpb := make([]byte, 0);
+	updated_before_print := 1;
+
+	for {
+		_, err = fd.Read(chars);
+
+		idx := bytes.IndexByte(chars, '\n');
+		if (idx != -1) {
+
+			tmpb = append(tmpb, chars[:idx]...);
+			fmt.Printf("read: %s\n", string(tmpb));
+			tmpb = tmpb[:0];
+			tmpb = append(tmpb, chars[idx+1:]...);
+			updated_before_print = 0;
+		} else {
+
+			tmpb = append(tmpb, chars...);
+			updated_before_print = 1;
+		}
+		if (err == io.EOF) {
+
+			break;
+		}
+
+	}
+	if (updated_before_print == 1) {
+
+		fmt.Printf("read: %s\n", string(tmpb));
+	}
+
+	return;
+
 }
