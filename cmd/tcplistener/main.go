a15c220cf3975b1557fb7394af2758800be28968
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-05-17 19:18:24 -0400

    Update line parses to use TCP connections instead

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,56 +2,11 @@ package main
 
 import( 
 	"fmt"
-	"io"
-//	"log"
 	"net"
-	"bytes"
+	"github.com/shreyasganesh0/TheStartup/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <- chan string {
-
-	ch := make(chan string);
-	go func() {
-
-		defer close(ch);
-		chars := make([]byte, 8);
-		tmpb := make([]byte, 0);
-
-		for {
-			n, err := f.Read(chars);
-
-			if (n > 0) {
-
-				idx := bytes.IndexByte(chars, '\n');
-				chunk := chars[:n]
-				if (idx != -1) {
-
-					tmpb = append(tmpb, chunk[:idx]...);
-					ch <- string(tmpb)
-					tmpb = tmpb[:0];
-					tmpb = append(tmpb, chunk[idx+1:]...);
-				} else {
-
-					tmpb = append(tmpb, chunk...);
-				}
-			}
-
-			if (err == io.EOF) {
-				break;
-			}
-
-		}
-		if len(tmpb) > 0 {
-
-			ch <- string(tmpb);
-		}
-	}()
-
-	return ch;
-}
-
 func main() {
-
 
 	listener, err := net.Listen("tcp4", "127.0.0.1:42069")
 	if (err != nil) {
@@ -70,12 +25,18 @@ func main() {
 
 		//log.Printf("created connection %z\n", tcp_conn);
 
-		ch := getLinesChannel(tcp_conn);
-		for msg := range ch {
 
-			fmt.Printf("%s\n", msg);
+		req, err_req := request.RequestFromReader(tcp_conn);
+		if (err_req != nil) {
+
+			fmt.Printf("Error creating file %z\n", err_tcp);
 		}
-		//log.Printf("closing connection %z\n", tcp_conn);
+
+		fmt.Printf("Request line:\n");
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method);
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget);
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion);
+
 	}
 
 
